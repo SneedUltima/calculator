5aa4302bc0149f75b1e1827ae89c53521780a835
Dylan <dylan.cobham@yahoo.com.au>
2022-09-04 17:15:30 +0930

    build: add functions for basic math operators

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(value1, value2) {
+    let answer = value1 + value2;
+    return answer
+}
+
+function subtract(value1, value2) {
+    let answer = value1 - value2;
+    return answer
+}
+
+function multiply(value1, value2) {
+    let answer = value1 * value2;
+    return answer
+}
+
+function divide(value1, value2) {
+    let answer = value1 / value2;
+    return answer
+}
